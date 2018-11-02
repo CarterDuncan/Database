a501a40f35698138003c0f1d32b7de2a3466c82d
cduncan <cduncan@scu.edu>
2018-11-02 14:57:57 -0700

    added project

---
+++ DatabaseNew/DatabaseNew/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DatabaseNew
+//
+//  Created by Carter Duncan on 11/2/18.
+//  Copyright © 2018 Santa Clara University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
